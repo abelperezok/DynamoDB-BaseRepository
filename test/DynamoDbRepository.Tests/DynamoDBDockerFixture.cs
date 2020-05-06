ac20775a394636616b215cd34f25fb9ebd2d29eb
Abel Perez Martinez <abelperezok@gmail.com>
2020-05-06 19:16:59 +0100

    Added integration tests

---
+++ test/DynamoDbRepository.Tests/DynamoDBDockerFixture.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Xunit;
+
+namespace DynamoDbRepository.Tests
+{
+    public class DynamoDBDockerFixture : IAsyncLifetime
+    {
+        private DockerClient _dockerClient;
+        private string _containerId;
+        private const string dynamodbLocalImage = "amazon/dynamodb-local";
+        public string TableName = "test_table";
+        public string ServiceUrl = "http://localhost:8000";
+        AmazonDynamoDBClient _dynamoDbClient;
+
+        public DynamoDBDockerFixture()
+        {
+            _dynamoDbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig { ServiceURL = ServiceUrl });
+            _dockerClient = new DockerClientConfiguration(new Uri(DockerApiUri())).CreateClient();
+        }
+        
+        #region [    IAsyncLifetime interface    ]
+
+        public async Task InitializeAsync()
+        {
+            await PullImage();
+            await StartContainer();
+
+            var createTableResponse = await CreateTableAsync(TableName);
+            Console.WriteLine($"{createTableResponse.TableDescription.TableStatus}");
+
+            await WaitUntilTableIsActive(TableName);
+        }
+        public async Task DisposeAsync()
+        {
+            if (_containerId != null)
+            {
+                await DisposeContainer();
+            }
+        }
+
+        #endregion
+
+
+        #region [    Docker container handling    ]
+
+        private string DockerApiUri()
+        {
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+            if (isWindows)
+            {
+                return "npipe://./pipe/docker_engine";
+            }
+
+            var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+            if (isLinux)
+            {
+                return "unix:///var/run/docker.sock";
+            }
+
+            throw new Exception("Was unable to determine what OS this is running on, does not appear to be Windows or Linux!?");
+        }
+
+        private async Task PullImage()
+        {
+            var imageCreateParams = new ImagesCreateParameters
+            {
+                FromImage = dynamodbLocalImage,
+                Tag = "latest"
+            };
+            await _dockerClient.Images.CreateImageAsync(imageCreateParams, null, new Progress<JSONMessage>());            
+        }
+
+        private async Task StartContainer()
+        {
+            var containerParams = new CreateContainerParameters
+            {
+                Image = dynamodbLocalImage,
+                ExposedPorts = new Dictionary<string, EmptyStruct>
+                {
+                    { "8000", default(EmptyStruct) }
+                },
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        { "8000", new List<PortBinding>{ new PortBinding { HostPort="8000" } } }
+                    },
+                    PublishAllPorts = true
+                },
+            };
+            var createContainerResponse = await _dockerClient.Containers.CreateContainerAsync(containerParams);
+            _containerId = createContainerResponse.ID;
+            Console.WriteLine($"Container {createContainerResponse.ID} created");
+            foreach (var warning in createContainerResponse.Warnings)
+            {
+                Console.WriteLine(warning);
+            }
+            var containerStarted = await _dockerClient.Containers.StartContainerAsync(_containerId, null);
+            Console.WriteLine($"Container {containerStarted} started");
+        }
+
+        private async Task DisposeContainer()
+        {
+            await _dockerClient.Containers.KillContainerAsync(_containerId, new ContainerKillParameters());
+            Console.WriteLine($"Container {_containerId} killed");
+
+            await _dockerClient.Containers.RemoveContainerAsync(_containerId, new ContainerRemoveParameters());
+            Console.WriteLine($"Container {_containerId} removed");
+        }
+
+        #endregion
+
+
+        #region [    DynamoDB table creation    ]
+
+        private async Task<CreateTableResponse> CreateTableAsync(string tableName)
+        {
+            var createTableReq = new CreateTableRequest
+            {
+                TableName = tableName,
+                KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement {AttributeName = "PK", KeyType = KeyType.HASH },
+                    new KeySchemaElement {AttributeName = "SK", KeyType = KeyType.RANGE }
+                },
+                AttributeDefinitions = new List<AttributeDefinition>{
+                    new AttributeDefinition { AttributeName = "PK", AttributeType = ScalarAttributeType.S },
+                    new AttributeDefinition { AttributeName = "SK", AttributeType = ScalarAttributeType.S },
+                    new AttributeDefinition { AttributeName = "GSI1", AttributeType = ScalarAttributeType.S }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput(5, 5),
+                GlobalSecondaryIndexes = new List<GlobalSecondaryIndex>
+                {
+                    new GlobalSecondaryIndex
+                    {
+                        IndexName = "GSI1",
+                        KeySchema = new List<KeySchemaElement>
+                        {
+                            new KeySchemaElement {AttributeName = "GSI1", KeyType = KeyType.HASH },
+                            new KeySchemaElement {AttributeName = "SK", KeyType = KeyType.RANGE }
+                        },
+                        Projection = new Projection
+                        {
+                            ProjectionType = ProjectionType.ALL
+                        },
+                        ProvisionedThroughput = new ProvisionedThroughput(5, 5)
+                    }
+                }
+            };
+
+            return await _dynamoDbClient.CreateTableAsync(createTableReq);
+        }
+
+        private async Task WaitUntilTableIsActive(string tableName)
+        {
+            var currentStatus = TableStatus.CREATING;
+            do
+            {
+                Console.WriteLine($"Checking if the Table is ready ... Currently is {currentStatus}");
+                var describeTable = await _dynamoDbClient.DescribeTableAsync(tableName);
+                currentStatus = describeTable.Table.TableStatus;
+                await Task.Delay(3000);
+            }
+            while (currentStatus != TableStatus.ACTIVE);
+            Console.WriteLine("Table ready !");
+        }
+
+        #endregion
+
+    }
+}
---
+++ test/DynamoDbRepository.Tests/RepositoryIntegrationTest.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using SampleDynamoDbRepository;
+using Xunit;
+
+namespace DynamoDbRepository.Tests
+{
+    public class RepositoryIntegrationTest : IClassFixture<DynamoDBDockerFixture>
+    {
+        private string _serviceUrl;
+        private string _tableName;
+
+        public RepositoryIntegrationTest(DynamoDBDockerFixture fixture)
+        {
+            _serviceUrl = fixture.ServiceUrl;
+            _tableName = fixture.TableName;
+        }
+
+        [Fact]
+        public async void TestUserRepository()
+        {
+            var repo = new UserRepository(_tableName, _serviceUrl);
+
+            var list = await repo.AllAsync();
+            Assert.Equal(0, list.Count);
+
+
+            var count = await repo.CountAsync();
+            Assert.Equal(0, count);
+
+
+            var item = new User { Id = "User0001", Name = "userA", FirstName = "User", LastName = "A", Email = "a@test.com" };
+            await repo.AddAsync(item);
+
+            list = await repo.AllAsync();
+            Assert.Equal(1, list.Count);
+
+            count = await repo.CountAsync();
+            Assert.Equal(1, count);
+
+            var found = await repo.FindByAsync("User0001");
+            Assert.NotNull(found);
+            Assert.Equal("User0001", found.Id);
+            Assert.Equal("userA", found.Name);
+            Assert.Equal("User", found.FirstName);
+            Assert.Equal("A", found.LastName);
+            Assert.Equal("a@test.com", found.Email);
+
+
+            found.Name = "userAA";
+            found.Email = "aa-aa@test.com";
+            found.FirstName = "UserUser";
+            found.LastName = "AA AA";
+            await repo.UpdateAsync(found);
+
+            var updated = await repo.FindByAsync("User0001");
+            Assert.NotNull(updated);
+            Assert.Equal("User0001", updated.Id);
+            Assert.Equal("userAA", updated.Name);
+            Assert.Equal("UserUser", updated.FirstName);
+            Assert.Equal("AA AA", updated.LastName);
+            Assert.Equal("aa-aa@test.com", updated.Email);
+
+            await repo.DeleteAsync("User0001");
+            var deleted = await repo.FindByAsync("User0001");
+            Assert.Null(deleted);
+
+            var emptyList = await repo.AllAsync();
+            Assert.Equal(0, emptyList.Count);
+
+            var emptyCount = await repo.CountAsync();
+            Assert.Equal(0, emptyCount);
+        }
+
+        [Fact]
+        public async void TestProjectRepository()
+        {
+            var repo = new ProjectRepository(_tableName, _serviceUrl);
+
+            var list = await repo.AllAsync();
+            Assert.Equal(0, list.Count);
+
+
+            var count = await repo.CountAsync();
+            Assert.Equal(0, count);
+
+
+            var item = new Project { Id = "Project0001", Name = "ProjectA", Description = "Project A" };
+            await repo.AddAsync(item);
+
+            var found = await repo.FindByAsync("Project0001");
+            Assert.NotNull(found);
+            Assert.Equal("Project0001", found.Id);
+            Assert.Equal("ProjectA", found.Name);
+            Assert.Equal("Project A", found.Description);
+
+            found.Name = "ProjectAA";
+            found.Description = "Project AA";
+            await repo.UpdateAsync(found);
+
+            var updated = await repo.FindByAsync("Project0001");
+            Assert.NotNull(updated);
+            Assert.Equal("Project0001", updated.Id);
+            Assert.Equal("ProjectAA", updated.Name);
+            Assert.Equal("Project AA", updated.Description);
+
+
+            await repo.DeleteAsync("Project0001");
+            var deleted = await repo.FindByAsync("Project0001");
+            Assert.Null(deleted);
+
+        }
+
+        [Fact]
+        public async void TestPersonRepository()
+        {
+            var repo = new PersonRepository(_tableName, _serviceUrl);
+
+            var list = await repo.AllAsync();
+            Assert.Equal(0, list.Count);
+
+
+            var count = await repo.CountAsync();
+            Assert.Equal(0, count);
+
+
+            var item = new Person { Id = 1, Name = "personA", FirstName = "Person", LastName = "A", Email = "pa@test.com", };
+            await repo.AddAsync(item);
+
+            var found = await repo.FindByAsync(1);
+            Assert.NotNull(found);
+            Assert.Equal(1, found.Id);
+            Assert.Equal("personA", found.Name);
+            Assert.Equal("Person", found.FirstName);
+            Assert.Equal("A", found.LastName);
+            Assert.Equal("pa@test.com", found.Email);
+
+
+            found.Name = "personAA";
+            found.FirstName = "PersonPerson";
+            found.LastName = "AA AA";
+            found.Email = "aa-aa@test.com";
+            await repo.UpdateAsync(found);
+
+            var updated = await repo.FindByAsync(1);
+            Assert.NotNull(updated);
+            Assert.Equal(1, updated.Id);
+            Assert.Equal("personAA", updated.Name);
+            Assert.Equal("PersonPerson", updated.FirstName);
+            Assert.Equal("AA AA", updated.LastName);
+            Assert.Equal("aa-aa@test.com", updated.Email);
+
+            await repo.DeleteAsync(1);
+            var deleted = await repo.FindByAsync(1);
+            Assert.Null(deleted);
+        }
+
+    }
+}
